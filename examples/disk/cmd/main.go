b7dfab8ab0347b405edb0601dae8a29e859395ad
Markus <mark@saiko.tech>
2024-12-03 13:56:11 +0000

    examples/disk: accept cli flags

--- examples/disk/cmd/main.go
+++ examples/disk/cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -12,14 +14,24 @@ import (
 )
 
 func main() {
-	m := disk.NewDiskGOTV("SuperSecureStringDoNotShare", "gotv_plus_binary")
+	addr := flag.String("addr", "localhost:8080", "address to listen on")
+	auth := flag.String("auth", "SuperSecureStringDoNotShare", "authentication token")
+	dataDir := flag.String("data", "gotv_plus_binary", "data directory")
+
+	flag.Parse()
+
+	if *auth == "" {
+		log.Fatal("auth argument is required")
+	}
+
+	m := disk.NewDiskGOTV(*auth, *dataDir)
 	app := fiber.New()
 	g := app.Group("/gotv") // /gotv
 	g.Use(logger.New())
 	gotv.SetupStoreHandlersFiber(m, g)
 	gotv.SetupBroadcasterHandlersFiber(m, g)
 
-	p := net.JoinHostPort("localhost", "8080")
+	p := net.JoinHostPort("0.0.0.0", "8080")
 
 	// Start server
 	log.Println("Start listening on:", p)
